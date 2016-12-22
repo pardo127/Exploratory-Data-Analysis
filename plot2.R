3bf6aad272cae82d37ce893373d843ea115b06a2
joser amon pardo <jrpardordgz@gmail.com>
2016-12-22 15:11:30 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,18 +1,9 @@
-## Getting full dataset
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+DATAdown <- "./data/household_power_consumption.txt"
+DATAread <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+SUBdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
-
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-
-## Plot 2
-plot(data$Global_active_power~data$Datetime, type="l",
-     ylab="Global Active Power (kilowatts)", xlab="")
-dev.copy(png, file="plot2.png", height=480, width=480)
+datetime <- strptime(paste(SUBdata$Date, SUBdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(SUBdata$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
