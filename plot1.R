5d3242c504716f8d89fa6f7943f8bd0140ea3d8e
joser amon pardo <jrpardordgz@gmail.com>
2016-12-22 15:04:15 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,20 +1,8 @@
-## Getting full dataset
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+DATAdown <- "./data/household_power_consumption.txt"
+DATAread <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+SUBdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
-
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-
-## Plot 1
-hist(data$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-
-## Saving to file
-dev.copy(png, file="plot1.png", height=480, width=480)
+globalActivePower <- as.numeric(SUBdata$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
